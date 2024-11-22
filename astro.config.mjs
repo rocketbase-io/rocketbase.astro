5b71539691ab4f93a82bd632f8d159d6cbec3854
Marten <marten@rocketbase.io>
2024-11-22 22:33:42 +0100

    feat: added dynamic luicide

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import {defineConfig} from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -22,5 +24,5 @@ export default defineConfig({
 
     integrations: [tailwind({
         applyBaseStyles: false,
-    }), mdx()]
+    }), mdx(), icon()]
 });

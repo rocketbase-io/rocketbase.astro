3a95e02c25913ac4c8a9d2b36c906072222d9ab4
Marten <marten@rocketbase.io>
2025-05-10 00:55:21 +0200

    feat: integrated imgix

--- src/lib/directus.ts
+++ src/lib/directus.ts
@@ -5,6 +5,6 @@ export const directus = createDirectus<CustomDirectusTypes>(import.meta.env.DIRE
     .with(staticToken(import.meta.env.DIRECTUS_API_KEY))
     .with(rest());
 
-export const buildImageUrl = (asset: { id?: string }) => {
-    return directus.url + "assets/" + asset.id;
+export const buildImageUrl = (asset: { id?: string }, width?: number) => {
+    return import.meta.env.IMGIX_URL + "assets/" + asset.id + (width ? "?w=" + width : "");
 }
